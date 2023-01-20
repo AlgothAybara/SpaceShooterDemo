c71ad82c323b1a6b1b7fa2b59ef0799e953fa6de
James Rigby <103948156+rigbyj9@users.noreply.github.com>
2023-01-20 12:33:56 -0700

    Update Ship.cs

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -6,20 +6,12 @@ public class Ship
 {
     public string shipName, shipClass;
        
-    
     //MaxFuel, currentFuel, feulRegenRate
-    public int fuelProperties, shieldProperties, armorProperties;
+    public (int, int, float, float) fuelProperties, shieldProperties, armorProperties;
 
     //maxSpeed, shipMass, turnRate, accelerationRate
     public (int, int, float, float) shipProperties;
    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-        
-    }
 }
 
 
