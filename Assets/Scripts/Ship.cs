48b1ea61658ed65fad16a4031477a2e774bcd903
James Rigby <103948156+rigbyj9@users.noreply.github.com>
2023-01-19 20:25:45 -0700

    Add files via upload

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship
+{
+    public string shipName, shipClass;
+       
+    
+    //MaxFuel, currentFuel, feulRegenRate
+    public int fuelProperties, shieldProperties, armorProperties;
+
+    //maxSpeed, shipMass, turnRate, accelerationRate
+    public (int, int, float, float) shipProperties;
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+}
+
+
+
