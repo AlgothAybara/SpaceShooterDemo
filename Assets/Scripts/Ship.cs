646ce4e4e7eb93358f07901281d376e8b740de96
rigbyj9 <rigbyj@byui.edu>
2023-01-21 18:01:07 -0700

    fixed spelling errors with inheritance

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ship
+public class Ship : MonoBehaviour
 {
     public string shipName, shipClass;
        
-    //MaxFuel, currentFuel, feulRegenRate
+    //MaxFuel, currentFuel, fuelRegenRate
     public (int, int, float, float) fuelProperties, shieldProperties, armorProperties;
 
     //maxSpeed, shipMass, turnRate, accelerationRate
