de04dc5d5044f27b2f50300a7a929356b9393503
ay0900356@byui.edu <64487231+AlgothAybara@users.noreply.github.com>
2023-01-24 08:48:33 -0700

    Created NPCData class. Inherits from PlayerData.

---
+++ Assets/Scripts/NPCData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCData : PlayerData
+{
+    //public List<NPC_AI> AI_list;
+    //public int currentAI; 
+}
