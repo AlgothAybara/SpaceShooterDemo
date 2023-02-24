c9a597944a85d51f354215ea49f2da97213b40a8
rigbyj9 <rigbyj@byui.edu>
2023-02-24 13:05:46 -0700

    debugged target indicators.

--- Assets/Scripts/targetIndicator.cs
+++ Assets/Scripts/targetIndicator.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 public class targetIndicator : MonoBehaviour
 {
    public Transform Target;
-   public float HideDistance;
-
+   public float HideDistanceMin; //Hide arrow when approaching certain distance from object
+   public float HideDistanceMax; //Hide arrow when x distance from object
 
     // Update is called once per frame
     void Update()
     {
-        var direction = Target.position - transform.position;
-        
-        if (direction.magnitude < HideDistance)
+        //If target doesn't exist, don't have the arrow appear
+        if (Target == null)
         {
             SetChildrenActive(false);
         }
+        var direction = Target.position - transform.position;
+        
+        //Have arrow visible while greater than min distance and less than max distance
+        if (direction.magnitude < HideDistanceMin)
+        {
+            SetChildrenActive(false);
+        }
+        else if (direction.magnitude > HideDistanceMax)
+        {
+            SetChildrenActive(false);
+        }
+        
+        //If distance is out of scope, don't have arrow appear
         else
         {
             SetChildrenActive(true); 
@@ -24,6 +36,7 @@ public class targetIndicator : MonoBehaviour
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
+    //This controls the visibility of the targeting arrows.
     void SetChildrenActive(bool value)
     {
          foreach (Transform child in transform)
