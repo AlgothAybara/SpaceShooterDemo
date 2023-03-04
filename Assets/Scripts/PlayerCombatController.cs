2c38191e58e0592c45b7de915d15019adc98fd85
ay0900356@byui.edu <64487231+AlgothAybara@users.noreply.github.com>
2023-03-03 23:43:35 -0700

    Added target switching to PlayerCombatController.cs

--- Assets/Scripts/PlayerCombatController.cs
+++ Assets/Scripts/PlayerCombatController.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class PlayerCombatController : CombatController
 {
+    private int targetIndex = 0;
     // Update is called once per frame
     void Update()
     {
         bulletPrefab = WeaponsList[0];
         // Checks if player presses or holds the shoot button
+        if (Input.GetKeyDown(KeyCode.Tab)){
+            var NPCs = this.GetComponentInParent<SystemManager>().spawnedNPCs;
+            if (NPCs.Count <= targetIndex || Target == null){
+                targetIndex = 0;
+            }
+
+            Target = NPCs[targetIndex];
+            targetIndex += 1;
+            Debug.Log(Target + " " + targetIndex);
+        } 
         if((Input.GetButton("Jump")||Input.GetButtonDown("Jump")))
         {   
             // calls the shoot function
