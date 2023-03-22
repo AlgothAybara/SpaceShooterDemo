ebea788e572042300275ed0802acd5c1b0d9f4a9
ay0900356@byui.edu <64487231+AlgothAybara@users.noreply.github.com>
2023-03-21 23:04:43 -0600

    Tweaked logic for calculations. Made chance out of 100.

--- Assets/Scripts/Structs/RouletteSelect.cs
+++ Assets/Scripts/Structs/RouletteSelect.cs
@@ -6,12 +6,15 @@ public class RouletteSelect : MonoBehaviour
 {
     // Biased select of items in a tuple list.
     public GameObject Select(List<GameObject> Plist, List<int> Ilist){
-        var max = Sum(Ilist);
+        // var max = Sum(Ilist);
+        var max = 100;
         var guess = Random.Range(0, max);
         var select = 0;
         for(var i = 0; i < Ilist.Count; i++){
             if (guess <= Ilist[i]){
                 select = i;
+                Debug.Log(max + " " + guess + " " + select);
+                break;
             }
         }
         return Plist[select];
